e065f20db188363d0de4200ba4e4d9f0abdd7c66
Michael Wright <8026090-mswright@users.noreply.gitlab.com>
2021-01-21 23:15:43 +0000

    Exercise 01Be

---
+++ Unit 01 - Introduction and Setup/01Be - Tools setup/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jan 21, 2021
+ *      Author: mw
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, world!" << endl;
+
+    return 0;
+}
+
+
+
