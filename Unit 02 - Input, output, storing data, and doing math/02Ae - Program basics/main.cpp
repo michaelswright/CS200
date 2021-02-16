60dd3171eebfa32de25cdfe825d01a5b85f5df91
Michael Wright <8026090-mswright@users.noreply.gitlab.com>
2021-02-16 05:21:03 +0000

    Upload New File

---
+++ Unit 02 - Input, output, storing data, and doing math/02Ae - Program basics/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#include "function.hpp"
+
+int main()
+{
+    Author();
+
+    cout << "Thanks for running!" << endl;
+    cout << "Bye!" << endl;
+
+    return 0;
+}
