4c5998e88907e9a6a18a51a6d0ac55443a3f93cc
Michael Wright <8026090-mswright@users.noreply.gitlab.com>
2021-02-16 05:20:41 +0000

    Upload New File

---
+++ Unit 02 - Input, output, storing data, and doing math/02Ae - Program basics/function.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+void Author()
+{
+    cout << "This program is by Michael" << endl
+         << "Favorite color: Green" << endl
+         << "Currently Playing: Take Five" << endl;
+}
