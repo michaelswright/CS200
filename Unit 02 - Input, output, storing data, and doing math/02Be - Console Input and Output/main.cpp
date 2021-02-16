56b4bdc03bd1d71c270456de2d25afbf0285f5ab
Michael Wright <8026090-mswright@users.noreply.gitlab.com>
2021-02-16 06:53:57 +0000

    Upload New File

---
+++ Unit 02 - Input, output, storing data, and doing math/02Be - Console Input and Output/main.cpp
@@ -0,0 +1,34 @@
+// 02Be Reciple Program by Michael Wright
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Chocolate Chip Recipe" << endl;
+    cout << "From https://www.allrecipes.com/recipe/10813/best-chocolate-chip-cookies/" << endl;
+    cout << endl;
+
+    cout << "INGREDIENTS" << endl;
+    cout << "----------------------------------" << endl;
+    cout << "1 cup butter, softened" << endl;
+    cout << "1 cup white sugar" << endl;
+    cout << "1 cup packed brown sugar" << endl;
+    cout << "2 eggs" << endl;
+    cout << "2 teaspoons vanilla extract" << endl;
+    cout << "1 teaspoon baking soda" << endl;
+    cout << "2 teaspoons hot water" << endl;
+    cout << "½ teaspoon salt" << endl;
+    cout << "3 cups all-purpose flour" << endl;
+    cout << "2 cups semisweet chocolate chips" << endl;
+    cout << "1 cup chopped walnuts" << endl;
+    cout << "----------------------------------" << endl;
+    cout << endl;
+    cout << endl;
+    cout << "DIRECTIONS" << endl;
+    cout << "1. Preheat oven to 350 degrees F (175 degrees C)." << endl;
+    cout << endl;
+    cout << "2. Cream together the butter, white sugar, and brown sugar until smooth. Beat in the eggs one at a time, then stir in the vanilla. Dissolve baking soda in hot water. Add to batter along with salt. Stir in flour, chocolate chips, and nuts. Drop by large spoonfuls onto ungreased pans." << endl;
+    cout << endl;
+    cout << "3. Bake for about 10 minutes in the preheated oven, or until edges are nicely browned." << endl;
+    return 0;
+}
