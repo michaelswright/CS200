c24ac97cbad248fdf899da896db9bb2c4b1da87f
Michael Wright <8026090-mswright@users.noreply.gitlab.com>
2021-02-20 00:18:17 +0000

    Add new file

---
+++ Unit 04 - Control flow - Looping with while loops and for loops/01-02-03-04p - Basic and Control Flow project/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    const float BASE_PRICE = 8.00;
+    const float SAUCE_PRICE = 0.53;
+    const float CHEESE_PRICE = 0.73;
+    const float MEAT_PRICE = 1.25;
+    const float VEGGIES_PRICE = 0.83;
+    string pizza = "";
+    float price = BASE_PRICE;
+    int menuChoice;
+    bool done = false;
+}
