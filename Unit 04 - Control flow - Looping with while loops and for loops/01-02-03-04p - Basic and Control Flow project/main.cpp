96855a1dd30aa30db153a268f8c53777ea03d4a6
Michael Wright <8026090-mswright@users.noreply.gitlab.com>
2021-02-20 00:36:56 +0000

    Update main.cpp

--- Unit 04 - Control flow - Looping with while loops and for loops/01-02-03-04p - Basic and Control Flow project/main.cpp
+++ Unit 04 - Control flow - Looping with while loops and for loops/01-02-03-04p - Basic and Control Flow project/main.cpp
@@ -16,4 +16,26 @@ int main()
     float price = BASE_PRICE;
     int menuChoice;
     bool done = false;
+
+    while (menuChoice != 6)
+    {
+
+        cout << "PIZZA MAKER" << endl;
+        cout << endl
+             << endl;
+
+        cout << "Current pizza: " << pizza << endl;
+        cout << "Price: $" << price << endl;
+        cout << endl;
+
+        cout << "TOPPINGS MENU" << endl;
+        cout << "1. Sauces" << endl;
+        cout << "2. Add Cheese" << endl;
+        cout << "3. Meats" << endl;
+        cout << "4. Veggies" << endl;
+        cout << "5. Reset pizza" << endl;
+        cout << "6. Checkout" << endl;
+        cout << ">> ";
+        cin >> menuChoice;
+    }
 }
