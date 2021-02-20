d0c009b82cb4095585bff535c6c4513e24aa9e7b
Michael Wright <8026090-mswright@users.noreply.gitlab.com>
2021-02-20 17:41:49 +0000

    Update main.cpp

--- Unit 04 - Control flow - Looping with while loops and for loops/01-02-03-04p - Basic and Control Flow project/main.cpp
+++ Unit 04 - Control flow - Looping with while loops and for loops/01-02-03-04p - Basic and Control Flow project/main.cpp
@@ -41,21 +41,116 @@ int main()
         {
         case 1:
             cout << "SAUCES MENU" << endl;
+            cout << endl;
+            cout << "1. Tomato" << endl;
+            cout << "2. Alfredo" << endl;
+            cout << "3. Barbecue" << endl;
+            cout << "4. Buffalo" << endl;
+            cout << ">> ";
+            cin >> menuChoice;
+
+            switch (menuChoice)
+            {
+            case 1:
+                pizza += "Tomato sauce, ";
+                price += SAUCE_PRICE;
+                break;
+            case 2:
+                pizza += "Alfredo sauce, ";
+                price += SAUCE_PRICE;
+                break;
+            case 3:
+                pizza += "Barbecue sauce, ";
+                price += SAUCE_PRICE;
+                break;
+            case 4:
+                pizza += "Buffalo sauce, ";
+                price += SAUCE_PRICE;
+                break;
+            default:
+                cout << "INVALID MENU OPTION" << endl;
+            }
             break;
         case 2:
             cout << "Extra cheese added" << endl;
+            pizza += "Cheese, ";
+            price += CHEESE_PRICE;
             break;
         case 3:
             cout << "MEATS MENU" << endl;
+            cout << endl;
+            cout << "1. Pepperoni" << endl;
+            cout << "2. Sausage" << endl;
+            cout << "3. Ham" << endl;
+            cout << "4. Chicken" << endl;
+            cout << ">> ";
+            cin >> menuChoice;
+
+            switch (menuChoice)
+            {
+            case 1:
+                pizza += "Pepperoni, ";
+                price += MEAT_PRICE;
+                break;
+            case 2:
+                pizza += "Sausage, ";
+                price += MEAT_PRICE;
+                break;
+            case 3:
+                pizza += "Ham, ";
+                price += MEAT_PRICE;
+                break;
+            case 4:
+                pizza += "Chicken, ";
+                price += MEAT_PRICE;
+                break;
+            default:
+                cout << "INVALID MENU OPTION" << endl;
+            }
             break;
         case 4:
             cout << "VEGGIES MENU" << endl;
+            cout << endl;
+            cout << "1. Mushrooms" << endl;
+            cout << "2. Black olives" << endl;
+            cout << "3. Pineapple" << endl;
+            cout << "4. Tomatoes" << endl;
+            cout << ">> ";
+            cin >> menuChoice;
+
+            switch (menuChoice)
+            {
+            case 1:
+                pizza += "Mushrooms, ";
+                price += VEGGIES_PRICE;
+                break;
+            case 2:
+                pizza += "Black olives, ";
+                price += VEGGIES_PRICE;
+                break;
+            case 3:
+                pizza += "Pineapple, ";
+                price += VEGGIES_PRICE;
+                break;
+            case 4:
+                pizza += "Tomatoes, ";
+                price += VEGGIES_PRICE;
+                break;
+            default:
+                cout << "INVALID MENU OPTION" << endl;
+            }
             break;
         case 5:
             cout << "Pizza reset" << endl;
+            pizza = "";
+            price = BASE_PRICE;
             break;
         case 6:
             cout << "CHECKOUT" << endl;
+            cout << "Pizza: " << pizza << endl;
+            cout << "Price: $" << price << endl;
+            cout << endl;
+            cout << "THANK YOU FOR ORDERING FROM BRICKOLINI PIZZERIA" << endl;
             done = true;
             break;
         default:
